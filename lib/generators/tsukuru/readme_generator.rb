fe853a6db812b895d4c09cb34b47d8580bf8c125
ichiroc <nobitahouse@gmail.com>
2025-03-06 23:35:00 +0900

    必要な情報を集めながら README を記載するように

--- lib/generators/tsukuru/readme_generator.rb
+++ lib/generators/tsukuru/readme_generator.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails/generators'
 require 'readline'
 
@@ -11,26 +13,25 @@ module Tsukuru
       'config/environments/staging.rb',
       'config/environments/development.rb',
       'config/environments/test.rb',
-      'db/schema.rb',
       'app/models/user.rb',
       'app/models/admin.rb',
-      'app/models/administrator.rb',
-    ]
+      'app/models/administrator.rb'
+    ].freeze
 
     def call
       puts <<~MSG
         プロジェクトについてのREADMEを作成します。
       MSG
 
-      contents = FileInspector.contents(INITIAL_FILES).map do
-        <<~CONTENT
+      generate(contents(file_paths: INITIAL_FILES))
+    end
 
-          ```#{_1.path}
-          #{_1.body}
-          ```
-        CONTENT
-      end
-      puts "実行中..."
+    private
+
+    def generate(contents, count = 0)
+      puts ''
+      puts 'Generating...'
+      puts ''
       response = client.chat(
         messages: [
           { role: 'system', content: <<~CONTENT },
@@ -44,13 +45,85 @@ module Tsukuru
 
             #{contents}
           CONTENT
-          { role: 'user', content: 'READMEを記載してください。' },
-        ]
+          { role: 'user', content: 'READMEを記載してください。' }
+        ],
+        tools: [
+          {
+            type: 'function',
+            function: {
+              name: 'contents',
+              description: <<~DESCRIPTION,
+                README を正確に書くために、プロジェクトのファイル内容を把握するための関数です。
+                引数には必要なファイルのパスを渡します。
+              DESCRIPTION
+              parameters: {
+                type: :object,
+                properties: {
+                  file_paths: {
+                    type: 'array',
+                    description: 'ファイルパスを Rails.root からの相対パスで指定してください。複数指定できます。ただしすでにファイルの内容を知っているパスを含めてはいけません。',
+                    items: {
+                      type: 'string',
+                      enum: Tsukuru::FileInspector.all_paths
+                    }
+                  }
+                },
+                required: ['file_paths']
+              }
+            }
+          },
+          {
+            type: 'function',
+            function: {
+              name: 'generate_readme',
+              description: <<~DESCRIPTION,
+                README ファイルを実際に生成するための関数です。
+                引数には Markdown 形式で README の内容を返します。
+              DESCRIPTION
+              parameters: {
+                type: :object,
+                properties: {
+                  content: {
+                    type: 'string',
+                    description: 'README の内容を Markdown 形式で指定してください。'
+                  }
+                },
+                required: ['content']
+              }
+            }
+          }
+        ],
+        tool_choice: 'required'
       )
-      puts response.dig('choices', 0, 'message', 'content')
+
+      tool_calls = response.dig('choices', 0, 'message', 'tool_calls')
+      function_name = tool_calls.dig(0, 'function', 'name')
+      arguments = JSON.parse(tool_calls.dig(0, 'function', 'arguments')).transform_keys(&:to_sym)
+      if function_name == 'contents' && count < 3
+        count += 1
+        generate(contents(**arguments), count)
+      elsif function_name == 'generate_readme'
+        generate_readme(**arguments)
+      else
+        raise 'なんで？'
+      end
     end
 
-    private
+    def generate_readme(content:)
+      File.open('README.md', 'w') { _1.write(content) }
+      puts 'README.md generated'
+    end
+
+    def contents(file_paths:)
+      Tsukuru::FileInspector.contents(file_paths).map do
+        <<~CONTENT
+
+          ```#{_1.path}
+          #{_1.body}
+          ```
+        CONTENT
+      end
+    end
 
     def client
       @client ||= Tsukuru::OpenAiClient.new
